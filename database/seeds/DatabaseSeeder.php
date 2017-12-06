5d7438ebb2ac4d4b87a4ced4565478f89b31b914
Hiram Guerrero <hiramg90@gmail.com>
2017-12-06 12:43:30 -0600

    Modying seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(ResourcesTableSeeder::class);
+        //$this->call(ResourcesTableSeeder::class);
         $this->call(TypesTableSeeder::class);
         $this->call(LanguagesTableSeeder::class);
     }
